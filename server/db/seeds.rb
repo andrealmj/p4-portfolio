613c1a5194a93a65481573ff91930a22793b02c7
andrealmj <andrealmj@live.com>
2019-02-26 10:48:54 +0800

    added seed. ignore prev

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -15,9 +15,11 @@ User.create!([
     {email: "andrealmj@live.com", password: "password", password_confirmation: "password", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil}
 ])
 
-# Dummy data for user 1 (user@user.com)
-Project.create(title: "Project 1", description: "My first project ever", project_link: "http://www.github.com", img_link: "http://www.pawculture.com/uploads/versions/scottishfold-2---x----670-440x---.jpg", :user_id => 1)
-Project.create(title: "Project 2", description: "My second project", project_link: "http://www.github.com", img_link: "https://i.pinimg.com/originals/a3/e1/6f/a3e16f68d8d04e3bfbfc602fe57f7f06.jpg", :user_id => 1)
+# Dummy data for user 8 (user@user.com)
+Project.create(title: "User proj 1", description: "My first project ever", project_link: "http://www.github.com", img_link: "https://www.mondragon-assembly.com/wp-content/uploads/2016/04/cosmetic1.jpg", :user_id => 8)
+Project.create(title: "User proj 2", description: "My 2nd project ever", project_link: "http://www.github.com", img_link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtbhVn1BUPW61by8FOCtRbzOJsPM_ioN4tF3zQR5w-Yqh4XPFK", :user_id => 8)
 
-# Dummy data for user 2 (test@test.com)
-Project.create(title: "test proj 1", description: "first project by test user", project_link: "http://www.github.com", img_link: "https://www.mondragon-assembly.com/wp-content/uploads/2016/04/cosmetic1.jpg", :user_id => 2)
+# Dummy data for user 9 (test@test.com)
+Project.create(title: "test Project 1", description: "first test project", project_link: "http://www.github.com", img_link: "http://www.pawculture.com/uploads/versions/scottishfold-2---x----670-440x---.jpg", :user_id => 9)
+Project.create(title: "test Project 2", description: "second test project", project_link: "http://www.github.com", img_link: "https://i.pinimg.com/originals/a3/e1/6f/a3e16f68d8d04e3bfbfc602fe57f7f06.jpg", :user_id => 9)
+
